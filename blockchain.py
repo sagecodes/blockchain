ce7bab92c5af62d7e20cfbfe7ca199032d1fa771
Sage Elliott <hello@sageelliott.com>
2018-06-08 16:02:23 -0700

    make create block function

--- blockchain.py
+++ blockchain.py
@@ -9,3 +9,11 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0'):
+
+    def create_block(self, proof, previous_hash):
+        block = {"index": len(self.chain) +1, 
+                  "timestamp": str(datetime.datetime.now()),
+                  "proof": proof,
+                  "previous_hash": previous_hash}
+        self.chain.append(block)
+        return block
