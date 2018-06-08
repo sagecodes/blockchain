6776e8b772d7da10f7e1faab52c840ddfd56321e
Sage Elliott <hello@sageelliott.com>
2018-06-07 20:10:33 -0700

    :Initial block

--- blockchain.py
+++ blockchain.py
@@ -2,3 +2,10 @@ import datetime
 import hashlib
 import json
 from Flask import flask, jsonify
+
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0'):
