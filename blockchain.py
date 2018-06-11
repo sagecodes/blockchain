f3feae5063176d0be7d54ff0946840e0415d9516
Sage Elliott <hello@sageelliott.com>
2018-06-10 22:09:56 -0700

    Create chain validation function

--- blockchain.py
+++ blockchain.py
@@ -21,7 +21,6 @@ x
     def get_previous_block(self):
         return self.chain[-1]
 
-
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
@@ -36,3 +35,19 @@ x
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexidigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] !='0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
