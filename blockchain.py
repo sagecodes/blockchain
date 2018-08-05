f953c2b020ec4d245c986cb14c9bd88a60ab12b9
Sage Elliott <hello@sageelliott.com>
2018-08-05 15:12:54 -0700

    Run flask app and verfy functions on postman

--- blockchain.py
+++ blockchain.py
@@ -88,3 +88,6 @@ def get_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
+
+# Run flask app
+app.run(host = '0.0.0.0', port = 5000)
